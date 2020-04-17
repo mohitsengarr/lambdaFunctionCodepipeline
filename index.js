5d7b74cd419aa040c587026fd8eceffbe7112d6e
Mohit Singh <mohitsingh.shiv@gmail.com>
2020-04-17 14:20:26 +0530

    code

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-exports.handler = () => {
-  return {
+var time = require("time");
+exports.handler = (event, context, callback) => {
+  var currentTime = new time.Date();
+  currentTime.setTimezone("America/Los_Angeles");
+  callback(null, {
     statusCode: "200",
-    body: "The time in Los Angeles is: ",
-  };
+    body: "The time in Los Angeles is: " + currentTime.toString(),
+  });
 };
